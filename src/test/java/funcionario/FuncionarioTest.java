eca94a34932d97d24eb8e441ffcbe4dc8d74597b
uailucasSilva <lucas.humberto@estudante.iftm.edu.br>
2023-03-30 21:07:56 -0300

    Criação das classes

---
+++ src/test/java/funcionario/FuncionarioTest.java
@@ -0,0 +1,12 @@
+package funcionario;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FuncionarioTest {
+@Test
+    public void testarContrutorPagamentoInvalido(){
+
+}
+
+}
