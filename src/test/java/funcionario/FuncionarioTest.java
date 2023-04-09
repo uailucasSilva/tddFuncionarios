6ced50195b87362966757d686cdc4c8d32d6c63d
uailucasSilva <lucas.humberto@estudante.iftm.edu.br>
2023-04-09 16:32:10 -0300

    Criação das classes

--- src/test/java/funcionario/FuncionarioTest.java
+++ src/test/java/funcionario/FuncionarioTest.java
@@ -5,4 +5,15 @@ import org.junit.jupiter.api.Test;
 
 class FuncionarioTest {
 
+    private Funcionario func;
+    @Test
+    public void testarContrutorPagamentoInvalido(){
+    String entradaNomeConstrutor = "Juraci";
+    int horasTrabalhadas = 50;
+    double valorHora = 130.00;
+
+    Assertions.assertThrows(IllegalArgumentException.class, () ->func = new Funcionario(entradaNomeConstrutor, horasTrabalhadas, valorHora));
+
+}
+
 }
